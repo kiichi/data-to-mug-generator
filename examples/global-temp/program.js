cf0613b425bb70dcb187ad95e5bc3be62e03cb33
Kiichi Takeuchi <info@xxki.com>
2022-10-22 16:50:29 -0400

    basic version 0.1

---
+++ examples/global-temp/program.js
@@ -0,0 +1,21 @@
+var zd = 0.5/12.0; // z-axis increment 0.5 mm height 12 steps to complete each circle
+var ed = 3.0; // extrusion
+var z = 1.0; // should be zero?
+var e = 0;
+var arr = [];
+var retArr = [];
+var converted = data.split('\n')
+                .filter(item=>item.indexOf('GISTEMP')>-1)
+                .map(item=>parseFloat(item.split(',')[2].replace('\r','')))
+                .reverse();
+for (var i=0; i<converted.length; i++) {
+    // data[i] is around -0.5 to +1
+    var modifier = converted[i] * 10;
+    var x = 90 + (50 + modifier) * Math.cos(i*Math.PI/6.0);
+    var y = 90 + (50 + modifier) * Math.sin(i*Math.PI/6.0);
+    z = z+zd;
+    e = e+ed;
+    retArr.push({"X":x, "Y":y, "Z":z,"E":e}); // do we need e
+    arr.push(`G1 X${x.toFixed(3)} Y${y.toFixed(3)} Z${z.toFixed(3)} E${e.toFixed(3)}`);
+}
+return retArr;
